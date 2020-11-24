abb05a25c9ba50c998dc7ce9fb43d1f1e765c86e
yq <11@qq.com>
2020-11-24 11:15:14 +0800

    123

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// 1.assets 
+import "./assets/css/reset.css"
+import "./assets/js/rem"
+
+// 2.components 公共组件
+
+// 3.pages 路由组件
+
+// 4.filters 过滤器
+
+// 5.utils/http.js ajax请求
+
+// 6.store 
+
+// 7.UI 框架
+
+// 8.stylus 样式
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
